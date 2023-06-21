7fcbfd088aa237ca0c3207c6ccdc10ac4745e2dc
Oluniyisrael <israeloluniyi2004@gmail.com>
2023-06-21 15:18:45 +0100

    Exported the check password functions

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -3,38 +3,31 @@ import CustomInput from '../customInputs/CustomInput'
 import CustomButton from '../customButton/CustomButton'
 import './Form.css'
 import database from '../../assets/jsons/database.json'
+import isCorrect from '../functions/isCorrect/IsCorrect'
 
 function Form() {
   const [username,setUserName] = useState('')
   const [password,setPassword] = useState('')
-  function isCorrect(){
-    for (let i = 0; i < database.length; i++) {
-      if(username === database[i].username && password === database[i].password){
-        console.log('true')
-        return true
-      }
-    }
-    console.log('false')
-    return false
-  }
-  // console.log(username)
 
   return (
     <form action="" id='form'>
         <p id='banner'>Welcome!! Please Input your username and password</p>
+
         <CustomInput 
         type='text'
         id='username'
         placeholder = 'Username'
         name= 'username'
         onchange={(e)=>{setUserName(e.target.value)}}/>
+
         <CustomInput
         type='password'
         id='password'
         placeholder = 'Password'
         name= 'password'
         onchange={(e)=>{setPassword(e.target.value)}}/>
-        <CustomButton value='Submit' onClick={isCorrect}/>
+        
+        <CustomButton value='Submit' onClick={()=>{isCorrect(database,username,password)}}/>
     </form> 
   )
 }
