efa327256bf9cb2513aa4ba09227684db6a1b0eb
Oluniyisrael <israeloluniyi2004@gmail.com>
2023-06-21 15:52:39 +0100

    Created new page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import LandingPage from './pages/landingPage/LandingPage';
 import './App.css';
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import UserPage from './pages/userPage/UserPage';
 
 function App() {
+  const isLoggedIn = true
   return (
     <div>
    <Router>
     <Routes>
       <Route path='/' element={<LandingPage/>}/>
+      {isLoggedIn && <Route path='/user' element={<UserPage/>}/>}
     </Routes>
    </Router>
 {/* errand sending App */}
