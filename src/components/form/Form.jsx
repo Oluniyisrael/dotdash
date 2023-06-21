f4056beed319ba8011d3650bdf251b7cc18d41f3
Oluniyisrael <israeloluniyi2004@gmail.com>
2023-06-21 15:00:18 +0100

    Starting passwords

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CustomInput from '../customInputs/CustomInput'
 import CustomButton from '../customButton/CustomButton'
 import './Form.css'
+import database from '../../assets/jsons/database.json'
 
 function Form() {
+  const [username,setUserName] = useState('')
+  const [password,setPassword] = useState('')
+  function isCorrect(){
+    for (let i = 0; i < database.length; i++) {
+      if(username === database[i].username && password === database[i].password){
+        console.log('true')
+        return true
+      }
+    }
+    console.log('false')
+    return false
+  }
+  // console.log(username)
+
   return (
     <form action="" id='form'>
         <p id='banner'>Welcome!! Please Input your username and password</p>
@@ -11,13 +26,15 @@ function Form() {
         type='text'
         id='username'
         placeholder = 'Username'
-        name= 'username'/>
+        name= 'username'
+        onchange={(e)=>{setUserName(e.target.value)}}/>
         <CustomInput
         type='password'
         id='password'
         placeholder = 'Password'
-        name= 'password'/>
-        <CustomButton value='Submit'/>
+        name= 'password'
+        onchange={(e)=>{setPassword(e.target.value)}}/>
+        <CustomButton value='Submit' onClick={isCorrect}/>
     </form> 
   )
 }
